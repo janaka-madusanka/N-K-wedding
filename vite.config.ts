069645801cd30a1c4a47457d23e278d34d3bbabb
Janaka Madusanka <160509773+janaka-madusanka@users.noreply.github.com>
2026-04-05 09:09:20 +0530

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/N-K-wedding/", // 👈 ADD THIS LINE
+  base: mode === "production" ? "/N-K-wedding/" : "/",
 
   server: {
     host: "::",
